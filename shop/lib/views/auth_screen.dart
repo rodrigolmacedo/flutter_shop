47273f713b4da00ff80184124583e2f97e485cdf
Rodrigo <7695045+rodrigolmacedo@users.noreply.github.com>
2020-07-28 13:24:26 -0300

    fix responsividade

--- shop/lib/views/auth_screen.dart
+++ shop/lib/views/auth_screen.dart
@@ -20,38 +20,47 @@ class AuthScreen extends StatelessWidget {
             end: Alignment.bottomRight,
           )),
         ),
-        Container(
-          width: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                margin: const EdgeInsets.only(bottom: 20.0),
-                padding:
-                    const EdgeInsets.symmetric(vertical: 8, horizontal: 50),
-                transform: Matrix4.rotationZ(-8 * pi / 180)..translate(-10.0),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.deepOrange.shade900,
-                    boxShadow: [
-                      BoxShadow(
-                        blurRadius: 8,
-                        color: Colors.black26,
-                        offset: Offset(0, 2),
-                      ),
-                    ]),
-                child: Text(
-                  'Minha Loja',
-                  style: TextStyle(
-                    color: Theme.of(context).accentTextTheme.headline6.color,
-                    fontSize: 45,
-                    fontFamily: 'Anton',
+        Center(
+          child: SingleChildScrollView(
+            child: Container(
+              width: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 45,
                   ),
-                ),
+                  Container(
+                    margin: const EdgeInsets.only(bottom: 20.0),
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 8, horizontal: 50),
+                    transform: Matrix4.rotationZ(-8 * pi / 180)
+                      ..translate(-10.0),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.deepOrange.shade900,
+                        boxShadow: [
+                          BoxShadow(
+                            blurRadius: 8,
+                            color: Colors.black26,
+                            offset: Offset(0, 2),
+                          ),
+                        ]),
+                    child: Text(
+                      'Minha Loja',
+                      style: TextStyle(
+                        color:
+                            Theme.of(context).accentTextTheme.headline6.color,
+                        fontSize: 45,
+                        fontFamily: 'Anton',
+                      ),
+                    ),
+                  ),
+                  AuthCard()
+                ],
               ),
-              AuthCard()
-            ],
+            ),
           ),
         ),
       ],
