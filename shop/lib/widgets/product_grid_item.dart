83ae8852b6e739308b66a9b77312940711b2a693
Rodrigo <7695045+rodrigolmacedo@users.noreply.github.com>
2020-07-28 13:38:06 -0300

    hero animation to product detail image

--- shop/lib/widgets/product_grid_item.dart
+++ shop/lib/widgets/product_grid_item.dart
@@ -21,10 +21,13 @@ class ProductGridItem extends StatelessWidget {
             Navigator.of(context)
                 .pushNamed(AppRoutes.PRODUCT_DETAIL, arguments: product);
           },
-          child: FadeInImage(
-            placeholder: AssetImage('assets\images\product-placeholder.png'),
-            image: NetworkImage(product.imageUrl),
-            fit: BoxFit.cover,
+          child: Hero(
+            tag: product.id,
+            child: FadeInImage(
+              placeholder: AssetImage('assets\images\product-placeholder.png'),
+              image: NetworkImage(product.imageUrl),
+              fit: BoxFit.cover,
+            ),
           ),
         ),
         footer: GridTileBar(
