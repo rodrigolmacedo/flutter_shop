f8c364eaf126ab08644477e636c417518b528f88
Rodrigo <7695045+rodrigolmacedo@users.noreply.github.com>
2020-07-28 13:59:22 -0300

    refactor to sliver

--- shop/lib/views/product_detail_screen.dart
+++ shop/lib/views/product_detail_screen.dart
@@ -8,41 +8,67 @@ class ProductDetailScreen extends StatelessWidget {
         ModalRoute.of(context).settings.arguments as Product;
 
     return Scaffold(
-        appBar: AppBar(title: Text(product.title)),
-        body: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              Container(
-                height: 300,
-                width: double.infinity,
-                child: Hero(
-                  tag: product.id,
-                  child: Image.network(
-                    product.imageUrl,
-                    fit: BoxFit.cover,
+        body: CustomScrollView(
+      slivers: <Widget>[
+        SliverAppBar(
+          expandedHeight: 300,
+          pinned: true,
+          flexibleSpace: FlexibleSpaceBar(
+              title: Text(
+                product.title,
+                softWrap: true,
+                textAlign: TextAlign.justify,
+              ),
+              background: Stack(
+                fit: StackFit.expand,
+                children: <Widget>[
+                  Hero(
+                    tag: product.id,
+                    child: Image.network(
+                      product.imageUrl,
+                      fit: BoxFit.cover,
+                    ),
                   ),
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                'R\$ ${product.price}',
-                style: TextStyle(color: Colors.grey, fontSize: 20),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                width: double.infinity,
-                child: Text(
-                  product.description,
-                  textAlign: TextAlign.center,
-                ),
-              )
-            ],
+                  const DecoratedBox(
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                      colors: [
+                        Color.fromRGBO(0, 0, 0, 0.6),
+                        Color.fromRGBO(0, 0, 0, 0)
+                      ],
+                      begin: Alignment(0, 0.8),
+                      end: Alignment(0, 0),
+                    )),
+                  ),
+                ],
+              )),
+        ),
+        SliverList(
+            delegate: SliverChildListDelegate([
+          SizedBox(
+            height: 10,
           ),
-        ));
+          Text(
+            'R\$ ${product.price}',
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.grey, fontSize: 20),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            width: double.infinity,
+            child: Text(
+              product.description,
+              textAlign: TextAlign.center,
+            ),
+          ),
+          SizedBox(
+            height: 1000,
+          )
+        ]))
+      ],
+    ));
   }
 }
